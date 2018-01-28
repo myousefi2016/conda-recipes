8ab0136ea94a84edcc9fb1f4fe6a8758406714dc
Rick Izzo <rizzo242@gmail.com>
2018-01-28 06:05:13 -0500

    try for all

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -1,8 +1,8 @@
 # Install and set up miniconda.
-if [ $TRAVIS_OS_NAME == "linux" ]; then sudo apt-get update && sudo apt-get install -yqq git wget \
-                                        build-essential libxt-dev libgl1-mesa-glx libgl1-mesa-dev libglapi-mesa libosmesa-dev \
-                                        libglu-dev libglw1-mesa elfutils gamin libglw1-mesa-dev mesa-utils xfstt ttf-liberation \
-                                        ttf-mscorefonts-installer xfonts-100dpi xfonts-75dpi; fi
+if [ $TRAVIS_OS_NAME == "linux" ]; then sudo apt-get update; fi
+if [ $TRAVIS_OS_NAME == "linux" ]; then sudo apt-get install -yqq git wget build-essential libxt-dev libgl1-mesa-glx libgl1-mesa-dev \
+                                        libglapi-mesa libosmesa-dev libglu-dev libglw1-mesa elfutils gamin libglw1-mesa-dev mesa-utils \ 
+                                        libglew-dev glew-utils libglewmx-dev libglew1.10 libglewmx-dbg libglewmx1.10; fi
 if [ $TRAVIS_OS_NAME == "linux" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; fi
 if [ $TRAVIS_OS_NAME == "osx" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh; fi
 if [ $TRAVIS_OS_NAME == "osx" ]; then cd $HOME && git clone https://github.com/phracker/MacOSX-SDKs.git && cd $TRAVIS_BUILD_DIR; fi
