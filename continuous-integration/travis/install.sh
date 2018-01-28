6a8d58bc7e0498e6e6d6d51847296cfc6201fb65
Rick Izzo <rizzo242@gmail.com>
2018-01-28 05:01:01 -0500

    [apskip] travis install updates

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -1,7 +1,8 @@
 # Install and set up miniconda.
 if [ $TRAVIS_OS_NAME == "linux" ]; then sudo apt-get update && sudo apt-get install -yqq git wget libxt-dev libgl1-mesa-glx libgl1-mesa-dev \
-                                         libglapi-mesa libosmesa-dev libglew-dev freeglut3 freeglut3-dev libfftw3-dev libwxgtk3.0-dev \
-                                         libpulse-dev mesa-utils libgl1-mesa-swx11 libgl1-mesa-swx11-dev xvfb libgl1-mesa-dri build-essential; fi
+                                        libosmesa-dev libglew-dev freeglut3 freeglut3-dev libfftw3-dev libwxgtk3.0-dev \
+                                        libpulse-dev mesa-utils xvfb libgl1-mesa-dri build-essential \
+                                        mesa-common-dev libpng-dev libjpeg-dev libxi-dev libxrandr-dev; fi
 if [ $TRAVIS_OS_NAME == "linux" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; fi
 if [ $TRAVIS_OS_NAME == "osx" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh; fi
 if [ $TRAVIS_OS_NAME == "osx" ]; then cd $HOME && git clone https://github.com/phracker/MacOSX-SDKs.git && cd $TRAVIS_BUILD_DIR; fi
